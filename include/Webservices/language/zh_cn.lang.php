0b5ff56be09201c6d89c2ff40623353a38eb707b
YUCHENG HU <yucheng.hu@ossez.com>
2013-01-30 21:51:28 -0500

    include 文件夹。

---
+++ include/Webservices/language/zh_cn.lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (C) 2006-2012 YUCHENG HU
+ *
+ * ---------------------------------------------------------
+ * OSSEZ (中国) 信息技术有限公司
+ * http://www.ossez.com
+ * http://src.ossez.com
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yucheng.hu@ossez.com
+ *
+ * ---------------------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件必须采用无 BOM 的 UTF-8 编码
+ * 2. 本语言文件为 ossez-5.4.0 分支，适用于 vTiger 5.4.0
+ * ---------------------------------------------------------
+ */
+
+$app_strings = array (
+	'Groups'=>'Groups',
+	'DocumentFolders'=>'Document Folders',
+	'Currency'=>'Currency',
+	'SINGLE_Groups'=>'Group',
+	'SINGLE_DocumentFolders'=>'Document Folder',
+	'SINGLE_Currency'=>'Currency',
+);
+
+$webservice_strings = array(
+	'LBL_INVALID_OLD_PASSWORD' => 'Invalid value given for old password.',
+	'LBL_NEW_PASSWORD_MISMATCH' => "New Password and confirm password don't match",
+	'LBL_DATABASE_QUERY_ERROR' => 'Database error while performing requested operation',
+	'LBL_CHANGE_PASSWORD_FAILURE' => 'Failed to change password',
+);
+
+?>
